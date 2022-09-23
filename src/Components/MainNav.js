06628dccb8f586b870b3d73bec3ad73079037f3f
Truong <duytruong5437@gmail.com>
2022-09-23 22:21:51 +0700

    add component

---
+++ src/Components/MainNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {Text} from '../context/Language';
+
+const MainNav = () => {
+  return (
+    <div className="sm:w-96 w-80 flex justify-around m-2 text-lg">
+      <NavLink to="/" exact>
+        <Text tid="homeView" />
+      </NavLink>
+      <NavLink to="/profile" exact>
+        <Text tid="profileView" />
+      </NavLink>
+      <NavLink to="/calendar" exact>
+        <Text tid="calendarView" />
+      </NavLink>
+    </div>
+  );
+};
+
+export default MainNav;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import MainNav from './MainNav'
+
+const Navbar = () => {
+  return (
+    <div className="container min-h-36 mx-auto">
+      <nav className="h-full flex justify-around flex-wrap">
+        <div className="flex items-center py-4">
+          <img
+            src="../assets/img/logo.png"
+            alt="welcome message"
+            className="w-6/12"
+            width="50"
+            height="20"
+          />
+        </div>
+        <div className="flex items-center justify-center flex-wrap">
+            <MainNav />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
