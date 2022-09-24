9fb5977a71ddfc8c8394027de53044aecb56493f
Truong <duytruong5437@gmail.com>
2022-09-24 22:33:03 +0700

    custom theme tailwind

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -46,6 +46,49 @@ const HomePage = () => {
           <h3 className="text-xl lg:px-14">
             <Text tid="section-2" />
           </h3>
+          <img
+            src="../assets/img/pics/kawai1.png"
+            alt=""
+            width="60%"
+            className="sm:absolute bottom-0 left-0 right-0 mx-auto w-24 sm:w-36
+          xl:w-52"
+          />
+        </div>
+        <div
+          className="sm:w-1/4 px-4 sm:px-0 bg-gradient-to-br from-lila-light 
+      to-lila-dark rounded-md shadow-md py-2 relative w-full m-2 sm:m-0 sm:h-96"
+        >
+          <h2 className="text-2xl py-4 text-blue-dark">
+            <Text tid="sectionHeader-3" />
+          </h2>
+          <h3 className="text-xl lg:px-14">
+            <Text tid="section-3" />
+          </h3>
+          <img
+            src="../assets/img/pics/kawai3.png"
+            alt=""
+            width="60%"
+            className="sm:absolute bottom-0 left-0 right-0 mx-auto w-24 sm:w-36
+          xl:w-52"
+          />
+        </div>
+        <div
+          className="sm:w-1/4 px-4 sm:px-0 bg-gradient-to-br from-gray-light 
+      to-gray-dark rounded-md shadow-md py-2 relative w-full m-2 sm:m-0 sm:h-96"
+        >
+          <h2 className="text-2xl py-4 text-gray-dark">
+            <Text tid="sectionHeader-4" />
+          </h2>
+          <h3 className="text-xl lg:px-14">
+            <Text tid="section-4" />
+          </h3>
+          <img
+            src="../assets/img/pics/kawai2.png"
+            alt=""
+            width="60%"
+            className="sm:absolute bottom-0 left-0 right-0 mx-auto w-24 sm:w-44
+          xl:w-56"
+          />
         </div>
       </section>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,42 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      lila: {
+        light: '#D1C1D7',
+        dark: '#B9A2C3',
+      },
+      black: '#000',
+      rose: {
+        light: '#F6CBD1',
+        dark: '#EEA0AA',
+      },
+      green: {
+        light: '#B4E9D6',
+        dark: '#8CDEC0',
+      },
+      blue: {
+        light: '#70ABC7',
+        dark: '#507b90',
+      },
+
+      white: {
+        pure: '#fff',
+        base: '#f9fafb',
+      },
+      gray: {
+        light: '#d1d5db',
+        dark: '#898D90',
+      },
+    },
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s ease-in-out infinite',
+      },
+      zIndex: {
+        '-10': '-10',
+      },
+    },
   },
   plugins: [],
 }
