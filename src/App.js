a5514a5b0f2d735114ee37627c1287e8d9ff3ad8
Truong <duytruong5437@gmail.com>
2022-09-25 15:34:37 +0700

    edit app com

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import './App.css';
+import React, {Suspense, lazy} from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Loading from './utils/Loading';
+
+import {LanguageProvider} from './context/Language';
+import LanguageOption from './Components/LanguageOption';
+import './index.css';
+
+const Home = lazy(() => import("./Components/HomePage"));
+const Calendar = lazy(() => import("./Components/Calendar"));
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+     <LanguageProvider>
+          <div className='h-screen bg-gradient-to-br from-lila-light  to-rose-light'>
+              <Navbar />
+              <div className='container mx-auto'>
+                  <LanguageOption />
+              </div>
+              <Suspense fallback={<Loading />}>
+                  <Switch>
+                       <Route exact path="/">
+                            <Home />
+                       </Route>
+                       <Route exact path="/calendar">
+                           <Calendar />
+                       </Route>
+                  </Switch>
+              </Suspense>
+          </div>
+     </LanguageProvider>
   );
 }
 
