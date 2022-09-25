34c2e6aa988afaa444a06bc39d2c886ef274985a
Truong <duytruong5437@gmail.com>
2022-09-25 14:46:09 +0700

    add component calendar

---
+++ src/Components/Calendar/ReminderList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Reminder from './Reminder';
+import {Text} from '../../context/Language'
+
+const ReminderList = (props) => {
+    const { reminders, setReminders, handleDeleteClick } = props;
+  return (
+    <div className="w-96">
+    {!reminders ||
+      (reminders.length === 0 && (
+        <div
+          className="h-full flex items-center flex-col px-6 relative
+        justify-around"
+        >
+          <p>
+            <Text tid="calendarWarning" />
+          </p>
+          <img src="/img/pics/kawai2.png" alt="" />
+        </div>
+      ))}
+    <ul>
+      {reminders.map((reminder) => (
+        <Reminder
+          key={reminder.id}
+          reminder={reminder}
+          reminders={reminders}
+          setReminders={setReminders}
+          handleDeleteClick={handleDeleteClick}
+        />
+      ))}
+    </ul>
+  </div>
+  )
+}
+
+export default ReminderList
--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -3,9 +3,64 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { v4 as uuid } from 'uuid';
 import {LanguageContext} from '../../context/Language';
-import Time from '../../utils/Time'
+import Time from '../../utils/Time';
+
+import ReminderList from './ReminderList';
 
 const CalendarView = () => {
+    const [reminder, setReminder] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [reminderDate, setReminderDate] = useState(new Date());
+    const [reminders, setReminders] = useState(() => {
+        const savedReminders = localStorage.getItem('reminders');
+        if (savedReminders) {
+          return JSON.parse(savedReminders);
+        }
+        return [];
+      });
+      const [showModal, setShowModal] = useState(false);
+      const handleClose = (e) => {
+        e.preventDefault();
+        setShowModal(false);
+        setReminder('');
+      };
+      const handleShow = () => setShowModal(true);
+      const {dictionary} = useContext(LanguageContext);
+      useEffect(() => {
+        localStorage.setItem('reminders', JSON.stringify(reminders));
+      }, [reminders]);
+
+      const handleReminderSubmitForm = (e) =>{
+          e.preventDefault();
+          if(reminder !== ""){
+            setReminders([
+                ...reminders, 
+                {
+                    id: uuid(),
+                    date: reminderDate,
+                    text: reminder.trim(),
+                    completed: false,
+                    show: false,
+                }
+            ])
+          }
+          setReminders('');
+      };
+      const handleDateTime = (e) => {
+        setDate(e.target.value);
+      };
+      const handleReminderChange = (e) => {
+        setReminder(e.target.value);
+      };
+      const onCalendarChange = (e) => {
+          setDate(e)      
+    };
+    const handleDeleteClick = (id) => {
+        if (window.confirm(dictionary.reminderDelete)) {
+          const removeReminder = reminders.filter((item) => item.id !== id);
+          setReminders(removeReminder);
+        }
+      };
   return (
     <div>Calendar</div>
   )
