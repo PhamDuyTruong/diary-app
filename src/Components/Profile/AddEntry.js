1cbe52e9fac1bf61fc35b7874023660dc7a653b7
Truong <duytruong5437@gmail.com>
2022-09-26 13:55:30 +0700

    add form profile

--- src/Components/Profile/AddEntry.js
+++ src/Components/Profile/AddEntry.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const AddEntry = (props) => {
+  const {handleShow} = props;
   return (
-    <div>AddEntry</div>
+    <button type="submit" className='w-20 h-20 mr-4 rounded-full bg-blue-light shadow-2xl fixed bottom-4 right-5 sm:right-56  hover:animate-spin-slow flex justify-center items-center'
+       onClick={handleShow}
+    > 
+      <img src='../../assets/img/icons/plus.svg' alt='add' width="56" height="56"/>
+    </button>
   )
 }
 
--- src/Components/Profile/EntryForm.js
+++ src/Components/Profile/EntryForm.js
@@ -1,8 +1,112 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {LanguageContext} from '../../context/Language'
 
 const EntryForm = (props) => {
+  const {
+    entry,
+  onAddFormSubmit,
+  onAddInputChange,
+  onHandleDateChange,
+  onHandleMoodChange,
+  date,
+  title,
+  showModal,
+  onClose,
+  onTitleChange,
+  } = props;
+
+  const showHideModal = showModal ? "block" : "hidden";
+  const isInvalid = entry === '' || title === '';
+  const {dictionary} = useContext(LanguageContext);
   return (
-    <div>EntryForm</div>
+    <div
+      className={`container sm:w-3/5 mx-auto modal shadow-2xl h-3/5 sm:h-full 
+      py-8 mt-20 flex justify-center items-center bg-gradient-to-tr 
+      from-lila-dark to-rose-light rounded-md absolute z-30 inset-0 
+      ${showHideModal}`}
+    >
+      <form
+        onSubmit={onAddFormSubmit}
+        className="bg-white-pure max-w-4/5 p-6 rounded-md shadow-lg text-md
+        min-h-full bg-opacity-20 backdrop-blur-2xl border border-white-pure 
+        border-opacity-25"
+      >
+        <fieldset className="p-3 border border-white-pure rounded-md w-full">
+          <legend className="text-md p-2">{dictionary.entryFormLegend}</legend>
+          <label htmlFor="title">{dictionary.entryTitle}</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="sm:w-full py-2 pl-2 border border-gray-light rounded-md 
+            shadow-inner"
+            onChange={onTitleChange}
+            value={title}
+            required
+            placeholder="Add a title to this entry"
+          />
+          <div className="flex justify-around">
+            <label>
+              {dictionary.entryMood}
+              <select
+                onChange={onHandleMoodChange}
+                aria-label="your mood"
+                className="p-2 pl-2 rounded-md border w-24 border-gray-light 
+                sm:m-4 shadow-inner cursor-pointer"
+              >
+                <option>{dictionary.entryMood1}</option>
+                <option>{dictionary.entryMood2}</option>
+                <option>{dictionary.entryMood3}</option>
+                <option>{dictionary.entryMood4}</option>
+                <option>{dictionary.entryMood5}</option>
+              </select>
+            </label>
+            <label htmlFor="date" className="pl-2 rounded-md m-4 ">
+              {dictionary.formDate}
+              <input
+                type="date"
+                id="date"
+                name="date"
+                className="shadow-inner rounded-md ml-2 py-2"
+                value={date.toLocaleString()}
+                required
+                onChange={onHandleDateChange}
+              />
+            </label>
+          </div>
+          <label htmlFor="entry">
+            {dictionary.entryEntry}
+            <textarea
+              name="entry"
+              type="text"
+              id="entry"
+              value={entry}
+              onChange={onAddInputChange}
+              className="w-full h-44 py-2 border pl-2 border-gray-primary
+              rounded-md shadow-inner sm:resize-none"
+              required
+              placeholder="Write something..."
+            />
+          </label>
+          <button
+            type="submit"
+            aria-disabled="true"
+            className={`bg-green-dark w-full py-4 mt-2 rounded-md shadow-sm
+            uppercase ${isInvalid && 'opacity-50'}`}
+          >
+            {dictionary.entryButton}
+          </button>
+        </fieldset>
+      </form>
+      <button
+        type="button"
+        onClick={onClose}
+        className="absolute top-4 left-4 rounded-full w-10 h-10 flex
+        justify-center bg-lila-dark shadow-2xl items-center"
+      >
+        <img src="/img/icons/close.svg" alt="X" width="20" height="20" />
+      </button>
+    </div>
   )
 }
 
