4cb08df74a73f1a35a6414294c142113035f990d
Truong <duytruong5437@gmail.com>
2022-09-26 11:21:32 +0700

    add auth

---
+++ src/auth/auth-provider.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const AuthProvider = ({children}) => {
+    const history = useHistory();
+    const domain = 'dev-hgxnpkps.eu.auth0.com';
+    const clientId = 'rnlzJ89VAm21PiIp2yJFKrjiMRrerqv8';
+    const audience = 'https://dev-hgxnpkps.eu.auth0.com/api/v2/';
+    const onRedirectCallback = (appState) => {
+        history.push(appState?.returnTo || window.location.pathname);
+      };
+  return (
+    <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+    audience={audience}
+  >
+    {children}
+  </Auth0Provider>
+  )
+}
+
+export default AuthProvider;
