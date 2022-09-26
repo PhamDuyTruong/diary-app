fb12f24931c657c88554effbbf9b0c03b465dc12
Truong <duytruong5437@gmail.com>
2022-09-26 14:26:33 +0700

    add edit profile component

--- src/Components/Profile/EditEntry.js
+++ src/Components/Profile/EditEntry.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Text } from "../../context/Language";
 
 const EditEntry = (props) => {
-  return (
-    <div>EditEntry</div>
-  )
-}
+  const { currentEntry, setIsEditing, onEditInputChange, onEditFormSubmit } =
+    props;
 
-export default EditEntry
+  return (
+    <form
+      onSubmit={onEditFormSubmit}
+      className="bg-gray-light w-full sm:w-1/2 py-3 px-6 h-96 rounded-md 
+    shadow-lg text-md z-50 backdrop-blur-2xl absolute top-20 left-0 right-0 
+    sm:mx-auto"
+    >
+      <fieldset className="p-3 border border-white rounded-md w-full">
+        <legend className="text-md p-2">
+          <Text tid="entryEdit" />
+        </legend>
+        <textarea
+          name="editEntry"
+          type="text"
+          aria-label="edit Entry"
+          rows="8"
+          className="w-full resize-none pl-2"
+          value={currentEntry.text}
+          onChange={onEditInputChange}
+        />
+      </fieldset>
+      <div className="flex justify-around">
+          <button type="submit" onClick={onEditFormSubmit}  className="bg-blue-dark px-3 py-3 mt-2 text-white-pure
+        rounded-full shadow-md uppercase">
+              <Text  tid="entryUpdate"/>
+          </button>
+          <button type="button"
+             onClick={() => setIsEditing(false)}
+             className="bg-white-pure px-3 py-3 mt-2 rounded-full shadow-md 
+             uppercase border border-blue-dark text-blue-dark"
+          >
+              <Text tid="EntryCancel"/>
+          </button>
+      </div>
+    </form>
+  );
+};
+
+export default EditEntry;
--- src/Components/Profile/EntryList.js
+++ src/Components/Profile/EntryList.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Entries from './Entries';
+import {Text} from '../../context/Language'
 
 const EntryList = (props) => {
+   const {entries, setEntries, handleEditClick, handleDeleteClick} = props;
 
   return (
-    <div>EntryList</div>
+    <>
+    {!entries ||
+      (entries.length === 0 && (
+        <div
+          className="mx-auto h-full flex items-center flex-col px-6 relative 
+        justify-around"
+        >
+          <p>
+            <Text tid="entryWarning" />
+          </p>
+          <img src="../../assets/img/pics/kawai3.png" alt="kawai-3" width="75%" />
+        </div>
+      ))}
+    {entries.map((entry) => (
+      <Entries
+        key={entry.id}
+        entry={entry}
+        entries={entries}
+        setEntries={setEntries}
+        handleEditClick={handleEditClick}
+        handleDeleteClick={handleDeleteClick}
+      />
+    ))}
+  </>
   )
 }
 
